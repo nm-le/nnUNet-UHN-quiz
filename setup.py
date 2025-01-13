6f6b8f3667943fb54c001df443f6334ee41d5d30
Minh Le <ngocminhle318@gmail.com>
2025-01-12 21:21:55 -0500

    add dataset back

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(
+        name="nnUNet-UHN-quiz",
+        version="0.1.0",
+        packages=setuptools.find_packages(exclude=["dataset", "dataset.*"]),
+    )
