71d7c5b915c5f86f64cfa1cb555fb4e8b9ebdcc5
Minh Le <ngocminhle318@gmail.com>
2025-01-12 21:22:20 -0500

    add train.sh

---
+++ train.sh
@@ -0,0 +1,5 @@
+export nnUNet_raw="/Users/minhle/Documents/3rd Year/UHN-MedImg3D-ML-quiz/dataset/nnUnet_raw"
+export nnUNet_preprocessed="/Users/minhle/Documents/3rd Year/UHN-MedImg3D-ML-quiz/dataset/nnUnet_preprocessed"
+export nnUNet_results="/Users/minhle/Documents/3rd Year/UHN-MedImg3D-ML-quiz/dataset/trained"
+
+nnUNetv2_plan_and_preprocess -d 1 --verify_dataset_integrity
